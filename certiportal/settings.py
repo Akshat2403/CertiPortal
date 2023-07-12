163eb0e353b1851824a756b7b8f89d04ace20f38
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-07-12 09:23:49 +0530

    debug

--- certiportal/settings.py
+++ certiportal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5mbelm=$(h8e6a193hs#*^16*3ifif!d45mtss&nx+rsb!8l@a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('prod')
 
 ALLOWED_HOSTS = ['*']
 
